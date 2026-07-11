a1055e64189ce484f468e15388ce322252e7a384
Adarsh Pandey <adarsh24803@gmail.com>
2026-07-11 20:20:33 +0530

    Completed Hands-on 2 - Load Country from Spring Configuration XML

--- WEEK 3/spring-rest-handson/src/main/java/com/cognizant/springresthandson/SpringRestHandsonApplication.java
+++ WEEK 3/spring-rest-handson/src/main/java/com/cognizant/springresthandson/SpringRestHandsonApplication.java
@@ -1,13 +1,36 @@
 package com.cognizant.springresthandson;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cognizant.springresthandson.model.Country;
 
 @SpringBootApplication
 public class SpringRestHandsonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestHandsonApplication.class, args);
-	}
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringRestHandsonApplication.class);
 
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringRestHandsonApplication.class, args);
+
+        LOGGER.info("Inside main");
+
+        displayCountry();
+    }
+
+    private static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+    }
 }
